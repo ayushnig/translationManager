15e9aebff1d41f1ddf3f7a580e4eceed8b7add2c
Ayush Nigam <ayushnigamdps@yahoo.co.in>
2016-11-04 17:28:40 +0530

    Electron Reading JSON data

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 const electron = require('electron');
+const fs = require('fs');
+const path = require('path');
+var p = ( __dirname + '/static/unit.json');
+var q = ( __dirname + '/static');
 const {app, BrowserWindow, Menu} = require('electron');
 
 // Keep a global reference of the window object, if you don't, the window will
@@ -19,94 +23,25 @@ app.on('ready', function() {
 
   // and load the index.html of the app.
   mainWindow.loadURL('file://' + __dirname + '/index.html');
-/*
-  var application_menu = [
-    {
-      label: 'menu1',
-      submenu: [
-        {
-          label: 'Undo',
-          accelerator: 'CmdOrCtrl+Z',
-          role: 'undo'
-        },
-        {
-          label: 'Open',
-          accelerator: 'CmdOrCtrl+O',
-          click: () => {
-            electron.dialog.showOpenDialog({ properties: [ 'openFile', 'openDirectory', 'multiSelections' ]});
+
+   fs.readdir(q, 'utf-8', function (err, data) {
+          if(err){
+              console.log("An error ocurred reading the file :" + err.message);
+              return;
           }
-        },
-        {
-          label: 'submenu1',
-          submenu: [
-            {
-              label: 'item1',
-              accelerator: 'CmdOrCtrl+A',
-              click: () => {
-                mainWindow.openDevTools();
-              }
-            },
-            {
-              label: 'item2',
-              accelerator: 'CmdOrCtrl+B',
-              click: () => {
-                mainWindow.closeDevTools();
-              }
-            }
-          ]
-        }
-      ]
-    }
-  ];
-  if (process.platform == 'darwin') {
-    const name = app.getName();
-    application_menu.unshift({
-      label: name,
-      submenu: [
-        {
-          label: 'About ' + name,
-          role: 'about'
-        },
-        {
-          type: 'separator'
-        },
-        {
-          label: 'Services',
-          role: 'services',
-          submenu: []
-        },
-        {
-          type: 'separator'
-        },
-        {
-          label: 'Hide ' + name,
-          accelerator: 'Command+H',
-          role: 'hide'
-        },
-        {
-          label: 'Hide Others',
-          accelerator: 'Command+Shift+H',
-          role: 'hideothers'
-        },
-        {
-          label: 'Show All',
-          role: 'unhide'
-        },
-        {
-          type: 'separator'
-        },
-        {
-          label: 'Quit',
-          accelerator: 'Command+Q',
-          click: () => { app.quit(); }
-        },
-      ]
+          // Change how to handle the file content
+          console.log("The file content is : " + data);
     });
-  }
-
-  menu = Menu.buildFromTemplate(application_menu);
-  Menu.setApplicationMenu(menu);*/
-
+   
+    fs.readFile(p, 'utf-8', function (err, data) {
+          if(err){
+              console.log("An error ocurred reading the file :" + err.message);
+              return;
+          }
+          // Change how to handle the file content
+          console.log("The file content is : " + data);
+    });
+ 
   // Emitted when the window is closed.
   mainWindow.on('closed', function() {
     // Dereference the window object, usually you would store windows
